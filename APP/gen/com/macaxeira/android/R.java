7bb1fcb2ec5154dfa89bba19ce7cf4e82179228b
aluno <aluno@10.2.230.102>
2014-09-02 09:34:01 -0300

    Deletando Activity inutil

--- APP/gen/com/macaxeira/android/R.java
+++ APP/gen/com/macaxeira/android/R.java
@@ -44,7 +44,6 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_tela_produtos=0x7f030001;
-        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
