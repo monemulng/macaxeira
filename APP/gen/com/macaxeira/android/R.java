0e906cf0ee8ede338d1853fd0c42d1b49c5be0fd
GlendaMacedo <glendinha.macedo1@hotmail.com>
2014-09-04 11:46:10 -0300

    Tela subproduto

--- APP/gen/com/macaxeira/android/R.java
+++ APP/gen/com/macaxeira/android/R.java
@@ -32,14 +32,20 @@ public final class R {
         public static final int logo=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000f;
+        public static final int addProduto=0x7f08000d;
         public static final int batata=0x7f080003;
+        public static final int expandableListView1=0x7f08000b;
+        public static final int expandableListView2=0x7f08000c;
         public static final int imageButton3=0x7f080005;
         public static final int imageView1=0x7f080006;
         public static final int listaprodutos=0x7f080008;
         public static final int lupa=0x7f080004;
         public static final int mesa=0x7f080000;
         public static final int mesaNumero=0x7f080001;
+        public static final int okProduto=0x7f08000e;
+        public static final int preco=0x7f080009;
+        public static final int produto=0x7f08000a;
         public static final int progressBar1=0x7f080007;
         public static final int sanduba=0x7f080002;
     }
@@ -47,6 +53,7 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int activity_splash=0x7f030001;
         public static final int activity_tela_produtos=0x7f030002;
+        public static final int activity_tela_subprodutos=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -57,12 +64,16 @@ public final class R {
         public static final int Codigo=0x7f05000a;
         public static final int Mesa=0x7f050009;
         public static final int action_settings=0x7f050001;
+        public static final int addProduto=0x7f05000e;
         public static final int app_name=0x7f050000;
         public static final int bt3=0x7f050008;
         public static final int hello_world=0x7f050002;
         public static final int img1=0x7f050006;
         public static final int img2=0x7f050007;
         public static final int mesa=0x7f050005;
+        public static final int okProduto=0x7f05000f;
+        public static final int preco=0x7f05000c;
+        public static final int produto=0x7f05000d;
         public static final int title_activity_splash=0x7f05000b;
         public static final int title_activity_tela_batata=0x7f050004;
         public static final int title_activity_tela_sanduiches=0x7f050003;
