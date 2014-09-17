f77f72b3f4ee2b3cacf400c8f5345acedc1d13fe
Dealeer <victorluiz.bass@gmail.com>
2014-09-17 12:57:08 -0300

    Inserindo valores no BD e adicionando interface AdicionalDAO.

---
+++ APP/src/com/macaxeira/DAO/AdicionalDAO.java
@@ -0,0 +1,20 @@
+package com.macaxeira.DAO;
+
+import java.util.List;
+
+import com.macaxeira.model.Adicional;
+import com.macaxeira.model.Produto;
+
+public interface AdicionalDAO {
+	
+	public void createAdicional(Adicional adic);
+	
+	public List<Adicional> readAdicional();
+	
+	public void updateAdicional(Adicional adic);
+	
+	public void deleteAdicional(Adicional adci);
+	
+	public List<Adicional> buscarAdicionalPorProduto(Produto prod);
+
+}
---
+++ APP/src/com/macaxeira/DAO/AdicionalDAOImpl.java
@@ -0,0 +1,40 @@
+package com.macaxeira.DAO;
+
+import java.util.List;
+
+import com.macaxeira.model.Adicional;
+import com.macaxeira.model.Produto;
+
+public class AdicionalDAOImpl implements AdicionalDAO {
+
+	@Override
+	public void createAdicional(Adicional adic) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Adicional> readAdicional() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateAdicional(Adicional adic) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteAdicional(Adicional adci) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Adicional> buscarAdicionalPorProduto(Produto prod) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
