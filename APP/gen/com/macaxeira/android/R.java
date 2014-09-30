c609a8b7e3d5b0c37f9692dc4260972a9d63b6ed
GlendaMacedo <glendinha.macedo1@hotmail.com>
2014-09-29 22:45:26 -0300

    Criacao telaPedido activity e layout

--- APP/gen/com/macaxeira/android/R.java
+++ APP/gen/com/macaxeira/android/R.java
@@ -31,32 +31,40 @@ public final class R {
         public static final int logo=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
-        public static final int addProduto=0x7f08000c;
+        public static final int action_settings=0x7f080018;
+        public static final int addPedido=0x7f08000b;
+        public static final int addProduto=0x7f080013;
         public static final int batata=0x7f080003;
-        public static final int checkbox=0x7f080010;
+        public static final int checkbox=0x7f080017;
+        public static final int enviar=0x7f08000e;
+        public static final int finalizar=0x7f08000d;
         public static final int imageButton3=0x7f080005;
         public static final int imageView1=0x7f080006;
-        public static final int lista=0x7f08000b;
-        public static final int listaprodutos=0x7f080008;
+        public static final int lista=0x7f080012;
+        public static final int listaPedido=0x7f08000c;
+        public static final int listaprodutos=0x7f08000f;
         public static final int lupa=0x7f080004;
         public static final int mesa=0x7f080000;
         public static final int mesaNumero=0x7f080001;
-        public static final int okProduto=0x7f08000d;
-        public static final int preco=0x7f080009;
-        public static final int produto=0x7f08000a;
+        public static final int numMesa=0x7f080009;
+        public static final int numPedido=0x7f08000a;
+        public static final int okProduto=0x7f080014;
+        public static final int preco=0x7f080010;
+        public static final int preco1=0x7f080008;
+        public static final int produto=0x7f080011;
         public static final int progressBar1=0x7f080007;
         public static final int sanduba=0x7f080002;
-        public static final int text=0x7f08000f;
-        public static final int text1=0x7f08000e;
+        public static final int text=0x7f080016;
+        public static final int text1=0x7f080015;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_splash=0x7f030001;
-        public static final int activity_tela_produtos=0x7f030002;
-        public static final int activity_tela_subprodutos=0x7f030003;
-        public static final int child_row=0x7f030004;
-        public static final int parent_row=0x7f030005;
+        public static final int activity_tela_pedido=0x7f030002;
+        public static final int activity_tela_produtos=0x7f030003;
+        public static final int activity_tela_subprodutos=0x7f030004;
+        public static final int child_row=0x7f030005;
+        public static final int parent_row=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -72,11 +80,14 @@ public final class R {
         public static final int addProduto=0x7f05000f;
         public static final int app_name=0x7f050000;
         public static final int bt3=0x7f050009;
+        public static final int enviar=0x7f050015;
+        public static final int finalizar=0x7f050014;
         public static final int hello_world=0x7f050002;
         public static final int img1=0x7f050007;
         public static final int img2=0x7f050008;
         public static final int mesa=0x7f050006;
         public static final int okProduto=0x7f050010;
+        public static final int pedido=0x7f050013;
         public static final int preco=0x7f05000d;
         public static final int produto=0x7f05000e;
         public static final int title_activity_main_activity2=0x7f050011;
